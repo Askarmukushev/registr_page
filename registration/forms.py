29e54537f8b820376c7967f4e167d322c37a0797
root <amukushev@outlook.com>
2024-04-21 21:43:55 +0500

    dits for dark theme

--- registration/forms.py
+++ registration/forms.py
@@ -5,3 +5,7 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+class MyForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
